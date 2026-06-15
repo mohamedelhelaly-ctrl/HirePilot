99d29d994465217faa4b818674be76a4d6ef4a78
Ahmed Khaled <ahmed22012501@gmail.com>
2026-06-15 16:42:52 +0300

    Update user_crud.py

--- src/backend/models/crud/user_crud.py
+++ src/backend/models/crud/user_crud.py
@@ -1,4 +1,4 @@
-﻿from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, func
 from typing import Optional, List
 import bcrypt
